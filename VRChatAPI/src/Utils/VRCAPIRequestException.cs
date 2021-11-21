6914b7c608966e4485845456061319bd8641b45e
mueru <mueru15@gmail.com>
2021-11-21 00:43:59 -0800

    change: VRCAPIRequestException to public

--- VRChatAPI/src/Utils/VRCAPIRequestException.cs
+++ VRChatAPI/src/Utils/VRCAPIRequestException.cs
@@ -6,10 +6,10 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using VRChatAPI.Objects;
 
-namespace VRChatAPI.Utils
+namespace VRChatAPI.Exceptions
 {
 	[Serializable]
-	internal class VRCAPIRequestException : HttpRequestException
+	public class VRCAPIRequestException : HttpRequestException
 	{
 		public readonly ResponseMessage ErrorMessage;
 		public readonly HttpStatusCode StatusCode;
