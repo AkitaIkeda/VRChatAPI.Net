0626a2bebbb0fdcff86459d0b92b44de826c8434
mueru <mueru15@gmail.com>
2021-11-21 10:24:46 -0800

    fix: Path Parse

--- VRChatAPI/src/Objects/File/VRCFilePath.cs
+++ VRChatAPI/src/Objects/File/VRCFilePath.cs
@@ -9,18 +9,18 @@ namespace VRChatAPI.Objects
 	public class VRCFilePath : VRCPathBase, IParsable
 	{
 		public override string Endpoint => "file";
-		public EFileType FileType { get; set; }
+		public EFileType? FileType { get; set; }
 		protected override void ParseFromStringL(string[] ls)
 		{
-			if (ls.Length < 4)
-				throw new ArgumentException($"Arg does not have enough information");
 			base.ParseFromStringL(ls);
-			if (!Enum.TryParse<EFileType>(ls[3], out var r))
-				throw new ArgumentException($"FileType is invalid, got {ls[3]}, expected {Enum.GetNames(typeof(EFileType))}");
-			else FileType = r;
+			if (ls.Length >= 4 && Enum.TryParse<EFileType>(ls[3], out var r)) FileType = r;
+			else FileType = null;
 		}
-		public override string GetUrl() =>
-			$"{base.GetUrl()}/{FileType.ToString()}";
+		public override string GetUrl(){
+			var ret = base.GetUrl();
+			if(!(FileType is null)) ret += $"/{FileType.ToString()}";
+			return ret;
+		}
 
 		public static VRCFilePath Parse(string path){
 			var r = new VRCFilePath();
